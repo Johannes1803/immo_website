68578537ab2fa555b19bdcbdc83c64d8b6f84363
johannes <Johannes.Birk11@web.de>
2022-06-19 12:58:27 +0200

    Add minimal example ORM objects
    Add one line to broker table, and reference to it in property table

---
+++ property_orm.py
@@ -0,0 +1,42 @@
+from sqlalchemy.orm import declarative_base, relationship, sessionmaker
+from sqlalchemy import (
+    Column,
+    Integer,
+    Float,
+    Boolean,
+    String,
+    ForeignKey,
+    create_engine,
+)
+
+Base = declarative_base()
+
+
+class Broker(Base):
+    __tablename__ = "BROKER"
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String)
+    last_name = Column(String)
+
+
+class Property(Base):
+    # ToDo: add attributes which are non-primitive types
+    __tablename__ = "PROPERTY"
+    id = Column(Integer, primary_key=True)
+    size = Column(Float)
+    broker_id = Column(Integer, ForeignKey("BROKER.id"))
+    broker = relationship("Broker", backref="properties")
+
+
+if __name__ == "__main__":
+    engine = create_engine("sqlite:///./properties_trading.db", echo=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    broker = Broker(first_name="Dagobert", last_name="Duck")
+    session.add(broker)
+    property_1 = Property(size=88.5, broker=broker)
+
+    session.commit()
