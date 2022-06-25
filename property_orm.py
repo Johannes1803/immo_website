fcfceb44b85c31a8003e1272a47d3d60f307e0b9
johannes <Johannes.Birk11@web.de>
2022-06-25 18:16:36 +0200

    Rename property to estate
    to avoid name collision with built-in

--- property_orm.py
+++ property_orm.py
@@ -43,20 +43,20 @@ class AdStatusTakerPerspective(Enum):
     accepted = 5
 
 
-class AssociationTakerProperty(Base):
-    __tablename__ = "association_taker_property"
+class AssociationTakerestate(Base):
+    __tablename__ = "association_taker_estate"
     taker_id = Column(ForeignKey("agent.id"), primary_key=True)
-    property_id = Column(ForeignKey("property.id"), primary_key=True)
+    estate_id = Column(ForeignKey("estate.id"), primary_key=True)
     ad_status_taker_perspective = Column(SqlEnum(AdStatusTakerPerspective))
-    property = relationship("Property")
+    estate = relationship("estate")
 
     def __init__(
         self,
-        property: "Property",
+        estate: "estate",
         ad_status_taker_perspective: AdStatusTakerPerspective,
     ):
         super().__init__()
-        self.property = property
+        self.estate = estate
         self.ad_status_taker_perspective = ad_status_taker_perspective
 
 
@@ -81,7 +81,7 @@ class Broker(Agent):
     __mapper_args__ = {
         "polymorphic_identity": "broker",
     }
-    managed_properties = relationship("Property", back_populates="broker")
+    managed_properties = relationship("estate", back_populates="broker")
 
 
 class Taker(Agent):
@@ -90,12 +90,12 @@ class Taker(Agent):
     __mapper_args__ = {
         "polymorphic_identity": "taker",
     }
-    _association_taker_properties = relationship("AssociationTakerProperty")
+    _association_taker_properties = relationship("AssociationTakerestate")
     watched_properties = association_proxy(
         "_association_taker_properties",
-        "property",
-        creator=lambda args: AssociationTakerProperty(
-            property=args[0], ad_status_taker_perspective=args[1]
+        "estate",
+        creator=lambda args: AssociationTakerestate(
+            estate=args[0], ad_status_taker_perspective=args[1]
         ),
     )
 
@@ -103,10 +103,10 @@ class Taker(Agent):
         super().__init__(**kwargs)
 
 
-class Property(Base):
+class estate(Base):
     """A base class for rentals, houses for sale, etc."""
 
-    __tablename__ = "property"
+    __tablename__ = "estate"
     id = Column(Integer, primary_key=True)
     size = Column(Float)
     floor = Column(Integer)
@@ -118,18 +118,18 @@ class Property(Base):
     is_garden_included = Column(Boolean)
     energy_efficiency = Column(SqlEnum(EnergyEfficiency))
     status = Column(SqlEnum(AdStatus), default=AdStatus.created)
-    location = relationship("Location", back_populates="property", uselist=False)
+    location = relationship("Location", back_populates="estate", uselist=False)
     broker_id = Column(Integer, ForeignKey("agent.id"))
     broker = relationship("Broker", back_populates="managed_properties")
     type = Column(String(20))
 
     __mapper_args__ = {
         "polymorphic_on": type,
-        "polymorphic_identity": "property",
+        "polymorphic_identity": "estate",
     }
 
 
-class Rental(Property):
+class Rental(estate):
     base_rent = Column(Float)
     additional_costs = Column(Float)
     __mapper_args__ = {
@@ -148,8 +148,8 @@ class Location(Base):
     postal_code = Column(String)
     street = Column(String)
     street_number = Column(String)
-    property_id = Column(Integer, ForeignKey("property.id"))
-    property = relationship("Property", back_populates="location", uselist=False)
+    estate_id = Column(Integer, ForeignKey("estate.id"))
+    estate = relationship("estate", back_populates="location", uselist=False)
 
 
 if __name__ == "__main__":
@@ -161,7 +161,7 @@ if __name__ == "__main__":
 
     broker = Broker(first_name="Dagobert", last_name="Duck")
     session.add(broker)
-    property_1 = Rental(
+    estate_1 = Rental(
         size=88.5,
         floor=1,
         rooms=4,
@@ -175,7 +175,7 @@ if __name__ == "__main__":
         base_rent=950.34,
         additional_costs=35.34,
     )
-    session.add(property_1)
+    session.add(estate_1)
 
     location_1 = Location(
         lat=100.1,
@@ -184,11 +184,11 @@ if __name__ == "__main__":
         street="Downing Street",
         street_number="14b",
         postal_code="431",
-        property=property_1,
+        estate=estate_1,
     )
     session.add(location_1)
 
-    property_2 = Rental(
+    estate_2 = Rental(
         size=188.5,
         floor=2,
         rooms=4,
@@ -202,7 +202,7 @@ if __name__ == "__main__":
         base_rent=950.34,
         additional_costs=35.34,
     )
-    session.add(property_2)
+    session.add(estate_2)
 
     location_2 = Location(
         lat=90.1,
@@ -211,15 +211,15 @@ if __name__ == "__main__":
         street="Downing Street",
         street_number="14b",
         postal_code="431",
-        property=property_2,
+        estate=estate_2,
     )
     session.add(location_2)
 
     taker = Taker(first_name="John", last_name="Doe")
 
-    taker.watched_properties.append((property_2, AdStatusTakerPerspective.rejected))
+    taker.watched_properties.append((estate_2, AdStatusTakerPerspective.rejected))
     session.add(taker)
     session.commit()
 
-    for property_ in taker.watched_properties:
-        print(property_)
+    for estate_ in taker.watched_properties:
+        print(estate_)
